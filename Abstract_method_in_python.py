64f9e070666dde7d7d41c562067ac6e92336e75e
AMARNATH K <46416914+Amarnath9962@users.noreply.github.com>
2021-05-25 12:49:10 +0530

    Add files via upload

---
+++ Abstract_method_in_python.py
@@ -0,0 +1,38 @@
+# from abc import ABC,abstractmethod
+# class Computer(ABC):
+#     @abstractmethod
+#     def Process(self):
+#         pass
+# class New_laptop(Computer):
+#     def Process(self):
+#         print("Hi")
+#     def New_world(self):
+#         print("new")
+# New = New_laptop()
+# New.Process()
+# New.New_world()
+
+# from abc import ABC,abstractmethod
+#
+# class Animals(ABC):
+#     @abstractmethod
+#     def Wild_Animals(self):
+#         pass
+#
+# class Forest(Animals):
+#     def Wild_Animals(self,Ne):
+#         print("Lion is wild animal in the forest")
+#         # Ne.New()
+#         Ne.Soft_animal(Fo)
+#
+#     def New(self):
+#         print('This is the New Function ')
+#
+# class Soft_animals:
+#     def Soft_animal(self,F):
+#         print("Parrot is flying bird in the world ")
+#         F.New()
+# Fo = Forest()
+# # Fo.Wild_Animals
+# S = Soft_animals()
+# Fo.Wild_Animals(S)
